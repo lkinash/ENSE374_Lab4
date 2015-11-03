7b9da4116ad8e84c399ae99f4d3c6ff6ab4454dc
Lindsey Kinash <kinash.lindsey@gmail.com>
2015-11-03 11:41:44 -0600

    Initial class making based on the class diagram

---
+++ Animal/src/Plant.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Plant {
+
+	private ArrayList canBeEatenBy;
+	private String type;
+	private boolean eatenToday;
+	
+	
+	public void setCanBeEatenBy(String name)
+	{
+	
+		return;
+	}
+	
+	public String getCanBeEatenBy(String name)
+	{
+		return name;
+	}
+	
+	public void setType(String typeName)
+	{
+		
+	}
+	
+	public String getType()
+	{
+	
+		return type;
+	}
+	
+	public boolean getEatenToday()
+	{
+		return true;
+	}
+	
+	public void setEatenToday(boolean getsEaten)
+	{
+		return;
+	}
+	
+}
